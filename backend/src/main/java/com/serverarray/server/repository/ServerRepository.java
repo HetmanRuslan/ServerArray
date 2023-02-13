9ad96080376870e40cdef05317ee56a845fdecfa
HetmanRuslan <getman.ruslan2001@gmail.com>
2023-02-13 09:48:27 +0200

    Create and realize Server.java , ServerRepository, ServerService and ServerServiceImpl

---
+++ backend/src/main/java/com/serverarray/server/repository/ServerRepository.java
@@ -0,0 +1,8 @@
+package com.serverarray.server.repository;
+
+import com.serverarray.server.model.Server;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServerRepository extends JpaRepository<Server,Long> {
+    Server findByIpAddress(String ipAddress);
+}
